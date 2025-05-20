cdb0fc29be074c87694ab2ccae41b5f1c1eb0341
Antony <antonycampbell17@gmail.com>
2025-05-19 19:56:00 -0600

    creando la ventana principal junto con otras funciones

--- tetris.py
+++ tetris.py
@@ -16,6 +16,30 @@ def centrar_ventana(ventana):
     y = (ventana.winfo_screenheight() // 2) - (alto // 2)  # winfo_screenheight() obtiene el alto de la pantalla
     ventana.geometry(f'{ancho}x{alto}+{x}+{y}') # resposiciona la venrana
 
+########################################################################################################################
+
+"""
+Nombre: ventana_usuario
+Entrada: Ninguna
+Salida: Obtiene los datos del jugador
+Restricciones: Las necesarias para el correcto funcionamiento
+"""
+def ventana_usuario(): # se convertira en el apartado de estadisticas
+    ventana = Tk()
+    ventana.geometry("500x500")
+    ventana.resizable(0,0)
+    centrar_ventana(ventana)
+
+
+
+
+
+
+
+
+    ventana.mainloop()
+
+
 
 
 """
@@ -24,7 +48,7 @@ Entraeda: Ninguna
 Salida: La ventana inicial, iniciara el juego o finalizara el programa
 Restricciones: Las necesarias para el correcto funcionamiento
 """
-def ventana_inicio():
+def ventana_inicio(): # Tal vez cambien los nombres de las variables
     ventana_inicial = Tk()   
     ventana_inicial.geometry("500x300")
     ventana_inicial.resizable(0,0)
@@ -50,14 +74,32 @@ Salida:
 Restricciones:
 """
 def interfaz(ventana):
-
-    # Creacion de ventana principal
+    # Creacion de ventana principal 
     ventana.destroy()
     consola = Tk()
     consola.geometry("600x800")
     consola.resizable(0,0)
+    consola.config(bg="#A9A9A9")
     centrar_ventana(consola)
-    ############################### 
+
+    imagen = Image.open("gameboyBase.png")
+    imagen_tk = ImageTk.PhotoImage(imagen)
+
+    canvas_gameboy = Canvas(consola, width=300, height=300, bg="pink")
+    canvas_gameboy.pack(fill="both", expand=True)
+    canvas_gameboy.create_image(0, 0, anchor="nw", image=imagen_tk)
+    ###############################
+
+    canvas_pantalla = Canvas(canvas_gameboy, width=465, height=366, bg="pink")
+    canvas_pantalla.place(x=67, y=47)
+
+    
+
+
+
+
+
+
 
 
 
